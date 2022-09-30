718b2794df480e53ed01feb8cc8342bc85a074cc
Dallas-Mutch <dallas.mutch@my.jcu.edu.au>
2022-09-30 10:29:56 +1000

    Answered random import methods

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+"""
+What did you see on line 1?
+the smallest number you could have seen is 5 and the
+biggest 20. randint includes both end points
+
+what did you see on line 2?
+smallest number 3 and biggest number 9
+line 2 could not produce a 4 since it is adding by 2
+causing odd numbers to occur.
+
+what did you see on line 3?
+2.5 is the smallest and 5.5 is the highest. depending on rounding
+
+"""
+
+
+import random
+
+print(random.randint(1, 100))
