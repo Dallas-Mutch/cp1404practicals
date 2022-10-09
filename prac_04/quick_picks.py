057c19aefd4bf3e5ddcd19746d5af0a46d0bc81d
Dallas-Mutch <dallas.mutch@my.jcu.edu.au>
2022-10-09 18:01:58 +1000

    Fixing up row and column

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,11 @@
+import random
+MIN = 1
+MAX = 45
+quick_picks = [1, 2, 3, 4, 5]
+
+quick_pick = int(input("How many quick picks? "))
+for row in range(MAX):
+    i = random.randint
+    for column in range(1):
+    #     print(column)
+        print(row, column)
