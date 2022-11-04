21728fba5d94f9d0fe6fe126e141b699615e2f11
Dallas-Mutch <dallas.mutch@my.jcu.edu.au>
2022-11-04 17:08:48 +1000

    Added string function

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,7 +1,21 @@
 """CP1404 Programming II
-    Time estimate:
+    Time estimate: 2 days
     Time completed:
 """
+import datetime
+
 
 class Project:
-    pass
+    def __init__(self, name, date, priority, cost, completion):
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __str__(self):
+        return f"{self.name}, Start: {self.date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: {self.cost}, completion: " \
+               f"{self.completion}%"
+
+    def is_completed(self):
+        return self.completion == 100
