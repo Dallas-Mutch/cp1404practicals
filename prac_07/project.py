f1003cec0c49ddc344d3776d33c415adccc2ca80
Dallas-Mutch <dallas.mutch@my.jcu.edu.au>
2022-11-08 21:23:32 +1000

    Completed the sorting and added a test function

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,16 +6,26 @@ import datetime
 
 
 class Project:
-    def __init__(self, name, date, priority, cost, completion):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
-        self.date = date
+        self.start_date = start_date
         self.priority = priority
-        self.cost = cost
-        self.completion = completion
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
 
     def __repr__(self):
-        return f"{self.name}, Start: {self.date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: {self.cost}, completion: " \
-               f"{self.completion}%"
+        return f"{self.name}, Start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: {self.cost_estimate}, completion: " \
+               f"{self.completion_percentage}%"
 
     def is_completed(self):
-        return self.completion == 100
+        return self.completion_percentage == 100
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+def run_tests():
+    t1 = Project("Mow lawn", datetime.datetime.strptime("12/1/2012", "%d/%m/%Y"), 3, 3.0, 0)
+    print(t1)
+
+if __name__ == '__main__':
+    run_tests()
