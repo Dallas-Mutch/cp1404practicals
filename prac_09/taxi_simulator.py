f0255b71ce22ccae5b13a8a6deaf573bc00de3e3
Dallas-Mutch <dallas.mutch@my.jcu.edu.au>
2022-11-18 15:36:18 +1000

    Changed choose function to the menu, left the core in the function, completed drive choice

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,32 +1,48 @@
+from prac_06.car import Car
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
+    total_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
     print("Let's drive!")
     print("q)uit, c)hoose taxi, d)rive")
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
             choose_taxis(taxis)
+            choose = int(input("Choose your taxi: "))
+            try:
+                current_taxi = taxis[choose]
+            except IndexError:
+                print("Invalid taxi choice")
         elif choice == "D":
-            pass
+            if current_taxi:
+                current_taxi.start_fare()
+                distant_driven = float(input("Drive how far? "))
+                current_taxi.drive(distant_driven)
+                trip_amount = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_amount:.2f}")
+                total_bill += trip_amount
+            else:
+                print("You need to choose a taxi before driving")
         else:
             print("Invalid input")
-        print(f"Bill to date: {taxis[0]}")
+        print(f"Bill to date: {total_bill:.2f}")
         print("q)uit, c)hoose taxi, d)rive")
         choice = input(">>> ").upper()
-    print("Bye")
+
+    print(f"Bill to date: {total_bill}")
+    print("Taxis are now:")
+    choose_taxis(taxis)
 
 
 def choose_taxis(taxis):
     print("Taxis available:")
     for i, taxi in enumerate(taxis):
         print(i, taxi)
-    choose = int(input("Choose your taxi: "))
-    taxi = taxis[choose]
-    print(taxi)
 
 # def taxi_drive():
 
